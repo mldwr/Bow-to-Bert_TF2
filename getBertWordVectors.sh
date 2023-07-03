984148d44833b120fd6fea8cbc8eac248fc15f7a
braunschweig <waldemarhujo@gmail.com>
2023-07-03 21:59:33 +0200

    init

--- getBertWordVectors.sh
+++ getBertWordVectors.sh
@@ -3,10 +3,10 @@
 input_file=./bert_sentences.txt
 output_file=./bertWordVectors.jsonl
 
-BERT_BASE_DIR="$PRE_TRAINED_HOME/bert/uncased_L-12_H-768_A-12"
+BERT_BASE_DIR="../bert/uncased_L-12_H-768_A-12"
 bert_master=./bert_master
 
-pipenv run python $bert_master/extract_features.py \
+python $bert_master/extract_features.py \
   --input_file=$input_file \
   --output_file=$output_file \
   --vocab_file=$BERT_BASE_DIR/vocab.txt \
@@ -15,4 +15,3 @@ pipenv run python $bert_master/extract_features.py \
   --layers=-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-11,-12 \
   --max_seq_length=128 \
   --batch_size=8
-
